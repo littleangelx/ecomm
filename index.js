3fc4e232bf1cc77b313e938ad139320f341f403f
Rachel Kirkland <100070583+littleangelx@users.noreply.github.com>
2025-08-10 15:32:27 +0100

    hash and salt passwords

--- index.js
+++ index.js
@@ -1,38 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
 
 import usersRepo from "./repositories/users.js";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.send(`
-        <div>
-            <form method="POST">
-                <input name="email" placeholder="email" />
-                <input name="password" placeholder="password" />
-                <input name="passwordConfirmation" placeholder="password confirmation">
-                <button>Sign Up</button>
-            </form>
-        </div>
-    `);
-});
-
-app.post("/", async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-
-  const existingUser = await usersRepo.getOneBy({ email });
-  if (existingUser) {
-    return res.send("Email in use");
-  }
-  if (password !== passwordConfirmation) {
-    return res.send("Passwords must match");
-  }
-
-  res.send("Account created!");
-});
+app.use(
+  cookieSession({
+    keys: ["jseiogfvs23i2ji241j4kf"],
+  })
+);
 
 app.listen(3000, () => {
   console.log("Listening");
